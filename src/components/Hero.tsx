d98a827721b7028ee66e4b2e0e004c2806b45079
chiragksharma <chiragksharma15@gmail.com>
2024-08-25 09:31:31 +0530

    Config json for hero title

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,28 @@
 "use client";
 import React from 'react';
 import { motion } from 'framer-motion';
-
+import siteConfig from '@config/siteConfig.json';
 
 const Hero: React.FC = () => {
+    const { hero } = siteConfig.content;
+    const { heading, description, ctaText, ctaLink,highlightedText } = hero;
+
+    // const parts = heading.split(new RegExp(`(${highlightedText})`, 'gi'));
+
+    const words = heading.split(' ');
+    const thirdLastWord = words[words.length - 3];
+    const lastTwoWords = words.slice(-2).join(' ');
+    const mainPart = words.slice(0, -3).join(' ');
+
+
+
+
     return (
         <div className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 lg:items-start px-8 py-8 lg:py-20">
             <div className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 lg:items-start px-8 py-8 lg:py-20">
                 <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-center lg:text-left lg:items-start">
                 <h1 className="text-4xl font-extrabold lg:text-6xl tracking-tight md:mb-4 flex flex-col gap-3 items-center lg:items-start">
-                        Build your Landing page in <span className="relative inline-block">
+                        {/* Build your Landing page in <span className="relative inline-block">
                         <span className='relative whitespace-nowrap'>
                             hours, 
                             </span>
@@ -18,7 +31,30 @@ const Hero: React.FC = () => {
                             <span className="absolute bg-yellow-500 -left-2 -top-1 -bottom-1 -right-2 md:-left-3 md:-top-0 md:-bottom-0 md:-right-3 -rotate-1"></span>
                             <span className="relative text-yellow-950">not days</span>
                             </span>
-                    </span>
+                    </span> */}
+
+                    {/* {parts.map((part, index) => (
+                            <span key={index} className="relative inline-block">
+                                {part.toLowerCase() === highlightedText.toLowerCase() ? (
+                                    <span className='relative whitespace-nowrap '>
+                                        <span className={`absolute bg-yellow-500 -left-2 -top-1 -bottom-1 -right-2 md:-left-3 md:-top-0 md:-bottom-0 md:-right-3 -rotate-1`}></span>
+                                        <span className={`relative text-yellow-950`}>{part}</span>
+                                    </span>
+                                ) : (
+                                    <span>{part}</span>
+                                )}
+                            </span>
+                    ))} */}
+                    {mainPart} <span className="relative inline-block">
+                        <span className="relative whitespace-nowrap">
+                            {thirdLastWord}
+                            </span>
+                            <span className='relative whitespace-nowrap ml-4'>
+                            <span className="absolute bg-yellow-500 -left-2 -top-1 -bottom-1 -right-2 md:-left-3 md:-top-0 md:-bottom-0 md:-right-3 -rotate-1"></span>
+                            <span className="relative text-yellow-950">{lastTwoWords}</span>
+                            </span>
+                        </span>
+
                         
                 </h1>
                 <p className="text-lg opacity-80 leading-relaxed">The NextJS boilerplate with all you need to build your SaaS product Landing page. Super easy to customize. </p>
